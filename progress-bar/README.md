fe4e053e5e1065afd1a1b91e5d2de4f7eabddbf7
jjongyeonglee <jjongyeonglee@gmail.com>
2018-04-28 14:47:55 +0900

    Add prograssbar example

---
+++ progress-bar/README.md
@@ -0,0 +1,22 @@
+# Prograss Bar
+
+This is a simple Python script for displaying a text progress bar in the terminal.
+
+## Usage
+
+Download the script and give execute permission to the script.
+
+```
+$ chmod 755 prograssbar.py
+$ ./prograssbar.py
+
+or
+
+$ python prograssbar.py
+```
+
+## Screenshot
+
+![prograssbar screenshot_1](https://raw.github.com/jjongyeonglee/python-example/master/screenshots/IMG_05.png)
+
+![prograssbar screenshot_2](https://raw.github.com/jjongyeonglee/python-example/master/screenshots/IMG_06.png)
---
+++ progress-bar/progressbar.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+import sys
+import time
+
+class ProgressBar:
+
+    def __init__(self, maxval, bar_length=20):
+        self.maxval = maxval
+        self.bar_length = bar_length
+
+    def start(self, sleep_time):
+        for i in xrange(0, self.maxval + 1):
+            percent = float(i) / self.maxval
+            hashes = '#' * int(round(percent * self.bar_length))
+            spaces = ' ' * (self.bar_length - len(hashes))
+            time.sleep(sleep_time)
+            # output format ex: Running: [###   ] 20%
+            sys.stdout.write("\rRunning: [{0}] {1}%".format(hashes + spaces, int(round(percent * 100))))
+            # flush stdout buffer
+            sys.stdout.flush()
+        print "\nFinish!"
+
+if __name__ == "__main__":
+    # example program
+    progressBar = ProgressBar(100, 30).start(0.1)
+    #progressBar.start(0.1)
